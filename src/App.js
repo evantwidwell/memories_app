9ea9c619333ee53ecaf0eb487cbca08abf22ae56
evantwidwell <evanski@gmail.com>
2021-01-14 15:49:14 -0600

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ const App = () => {
   return(
     <Container maxWidth='lg'>
       <AppBar className = {classes.appBar} position="static" color="inherit">
-        <Typography className = {classes.heading} variant="h2" align="center">Memories
-        <img className = {classes.image}src={memories} alt="memories" height="60" ></img></Typography>
+        <Typography className = {classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className = {classes.image}src={memories} alt="memories" height="60" ></img>
       </AppBar>
       <Grow in>
         <Container>
